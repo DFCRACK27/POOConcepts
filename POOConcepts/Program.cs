988a3130589e4c0c05a0e16add25d26dc2fe29af
dfernandez <dfernandez@SOPORTE-LOURTEC>
2024-05-11 10:57:26 -0400

    Agregar archivos de proyecto.

---
+++ POOConcepts/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using POOConcepts;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+
+try
+{
+	Console.WriteLine(new Date(2022, 12, 7));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 2));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
