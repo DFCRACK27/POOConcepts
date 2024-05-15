b2dfdbd01a52cf4c50fb433ba8eba625831d33f6
dfernandez <df43405@gmail.com>
2024-05-15 17:33:53 -0400

    Employee´s class finished

--- POOConcepts/Program.cs
+++ POOConcepts/Program.cs
@@ -15,7 +15,7 @@ Employee employee1 = new SalaryEmployee()
 
 
 };
-Console.WriteLine(employee1);
+//Console.WriteLine(employee1);
 
 Employee employee2 = new CommissionEmployee()
 {
@@ -31,4 +31,51 @@ Employee employee2 = new CommissionEmployee()
 
 
 };
-Console.WriteLine(employee2);
+//Console.WriteLine(employee2);
+
+Employee employee3 = new HourlyEmployee()
+{
+    Id = 3,
+    FirstName = "Juan",
+    LastName = "Perez",
+    BirthDate = new Date(1997, 8, 11),
+    HiringDate = new Date(2015, 3, 25),
+    IsActive = true,
+    HourValue = 123456.56M,
+    Hours =12.23F
+
+
+
+};
+//Console.WriteLine(employee3);
+
+Employee employee4 = new BaseCommissionEmployee()
+{
+    Id = 4,
+    FirstName = "Marta",
+    LastName = "Ramos",
+    BirthDate = new Date(1985, 5, 15),
+    HiringDate = new Date(2010, 8, 21),
+    IsActive = true,
+    Base = 860.45M,
+    Sales = 58000M,
+    CommissionPercentaje = 0.015F 
+
+
+
+};
+//Console.WriteLine(employee4);
+
+ICollection<Employee> employees = new List<Employee>()
+{ employee1, employee2, employee3, employee4 };
+
+decimal payroll = 0;
+foreach (Employee employee in employees)
+{
+    Console.WriteLine(employee);
+    payroll+= employee.GetValueToPay();
+
+}
+
+Console.WriteLine("                                  ===================");
+Console.WriteLine($"Total                             {$"{payroll:C2}",18}");
