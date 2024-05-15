5f5e13776cdb3da55756bb0bf72cf59c44ee251a
dfernandez <df43405@gmail.com>
2024-05-15 16:38:25 -0400

    Employee, SalaryEmployee & CommissionEmployee finish

--- POOConcepts/Program.cs
+++ POOConcepts/Program.cs
@@ -1,19 +1,34 @@
-﻿
-
-using POOConcepts;
+﻿using POOConcepts;
 
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
-
-try
+Employee employee1 = new SalaryEmployee()
 {
-	Console.WriteLine(new Date(2022, 12, 7));
-	Console.WriteLine(new Date(1974, 9, 23));
-	Console.WriteLine(new Date(1985, 11, 2));
-}
-catch (Exception error)
-{
+    Id = 1,
+    FirstName = "Sandra",
+    LastName = "Martinez",
+    BirthDate = new Date(1990, 5, 25),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
 
-	Console.WriteLine(error.Message);
-}
+
+};
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2,
+    FirstName = "Patricia",
+    LastName = "Marin",
+    BirthDate = new Date(1997, 8, 11),
+    HiringDate = new Date(2015, 3, 25),
+    IsActive = true,
+    Sales = 32000000M,
+    CommissionPercentaje = 0.03F
+   
+
+
+};
+Console.WriteLine(employee2);
